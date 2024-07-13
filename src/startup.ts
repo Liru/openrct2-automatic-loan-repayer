e559b5cc02d3c0012b4b7bcfe5bcedf0b84ce913
Liru <theliru@gmail.com>
2024-07-12 20:08:33 -0400

    add actual implementation

--- src/startup.ts
+++ src/startup.ts
@@ -1,20 +1,147 @@
-function onClickMenuItem()
-{
-	// Write code here that should happen when the player clicks the menu item under the map icon.
+import {checkEvery, ConfigCheckInterval, getCheckEvery, getCheckEveryInt, getPluginEnabled, setCheckEvery, setPluginEnabled} from "./config";
 
-	console.log("Hello world!");
+function onClickMenuItem() {
+	// Write code here that should happen when the player clicks the menu item under the map icon.
+	openWindow();
+}
+
+var handle: Window | undefined = undefined;
+var repaymentType: ConfigCheckInterval = "tick";
+
+function openWindow() {
+	if (handle === undefined) {
+		handle = ui.openWindow({
+			classification: 'automatic-loan-repayer',
+			width: 256,
+			height: 64,
+			title: 'Automatic Loan Repayer - Options',
+
+			onClose: function () {
+				handle = undefined
+			},
+			widgets: [{
+				type: 'checkbox',
+				text: 'Enable auto repayment',
+				isChecked: getPluginEnabled(),
+				tooltip: 'Automatically repays loans if possible.',
+				x: 5,
+				y: 20,
+				width: 120,
+				height: 12,
+				onChange: function (isChecked: boolean) {
+					setPluginEnabled(isChecked);
+				},
+			}, {
+				type: 'label',
+				width: 150,
+				height: 12,
+				x: 18,
+				y: 35,
+				text: 'Attempt to repay every',
+			},
+			{
+				type: 'dropdown',
+				x: 210,
+				y: 35,
+				width: 40,
+				height: 12,
+
+				items: ['tick', 'day', 'month'],
+				selectedIndex: getCheckEveryInt(),
+				onChange: function (i: number) {
+					repaymentType = checkEvery[i];
+					setCheckEvery(repaymentType);
+					createSubscription()
+				}
+
+			},
+			]
+		});
+	}
+}
+
+// Yes, intentional. Money is measured in dimes; $1000 = 10,000 units.
+const ONE_THOUSAND_DOLLARS = 10_000
+
+var oldBankLoan: number = Number.MAX_VALUE
+
+function checkIfLoanTakenOut() {
+	if (park.bankLoan > oldBankLoan) {
+		// we took out a loan, stop autorepayment
+		park.postMessage({ type: 'money', text: 'Automatic loan repayment disabled due to loan being increased.' });
+		setPluginEnabled(false);
+	}
+
+	oldBankLoan = park.bankLoan
+}
+
+function attemptLoanPaybackMonthly() {
+	if (date.day === 1) {
+		attemptLoanPayback();
+	}
+}
+
+function attemptLoanPayback() {
+
+	checkIfLoanTakenOut();
+
+	if (!getPluginEnabled() || park.bankLoan === 0 || park.cash < ONE_THOUSAND_DOLLARS) {
+		return;
+	}
+
+	const payments = Math.floor(park.cash / ONE_THOUSAND_DOLLARS)
+	// honestly don't know how it could be _less than_ zero here, but I've seen weirder stuff in other codebases
+	if (payments <= 0) {
+		return;
+	}
+
+	context.executeAction('parksetloan', { value: oldBankLoan - (payments * ONE_THOUSAND_DOLLARS) }, (res) => {
+		const { error } = res;
+		if (error !== 0) {
+			console.log(res);
+		}
+	})
+
+	const loan = park.bankLoan
+	if (oldBankLoan !== 0 && loan === 0) {
+		park.postMessage({ type: 'money', text: 'Your loan has been paid off.' });
+		setPluginEnabled(false);
+	}
+
+	oldBankLoan = loan;
 }
 
 
-export function startup()
-{
+var subscription: IDisposable | undefined = undefined;
+
+function createSubscription() {
+	subscription?.dispose();
+	subscription = undefined;
+	const interval = getCheckEvery();
+	switch (interval) {
+		case "tick":
+			subscription = context.subscribe('interval.tick', attemptLoanPayback);
+			break;
+		case "day":
+			subscription = context.subscribe('interval.day', attemptLoanPayback);
+			break;
+		case "month":
+			subscription = context.subscribe('interval.day', attemptLoanPaybackMonthly);
+			break;
+	}
+}
+
+export function startup() {
 	// Write code here that should happen on startup of the plugin.
 
+	// Currently, make it only work for single player
+	// TODO: Implement for multiplayer?
+	if (network.mode !== 'none') { return; }
 
+	createSubscription()
 
 	// Register a menu item under the map icon:
-	if (typeof ui !== "undefined")
-	{
-		ui.registerMenuItem("My plugin", () => onClickMenuItem());
+	if (typeof ui !== "undefined") {
+		ui.registerMenuItem("Automatic Loan Repayer", () => onClickMenuItem());
 	}
 }
